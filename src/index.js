296764ba16c66803c7e1eca724119e06684a9e4f
Domnic Omondi <domooh9@gmail.com>
2022-06-20 01:03:32 +0300

    fetch data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // Your code here
-
 /* Algorithm
 command Json--server --watch db.json ==> "open JSON server"
 use a fetch(url) to access data from json file
@@ -10,49 +9,26 @@ use catch method to tack any errors and print in console
 Use PATCH to reset character votes in json file to adjusted votes
 create function addToCharacterBar that takes each character as a parameter 
 create a <span> element for each character and input the names into it, add span to the character bar
-Use POST method to add new character
+Use  using POST method to addnew character
 
  */
-const url = 'http://localhost:3000/characters'
-const characterBar = document.querySelector('#character-bar')
-const characterInfo = document.querySelector('#detailed-info')
-let characterVotes = document.querySelector('#vote-count')
-
-//make a fetch request using URL to access data from json file
-fetch(url)
-//convert that data into javascript so we can work with it
-.then(res => res.json())
-.then(characters => {
-    //when you refresh, set the characterInfo to first character
-    const name = characterInfo.querySelector('#name')
-    const image = characterInfo.querySelector('#image')
-    name.textContent = characters[0].name
-    image.src = characters[0].image
-    characterVotes.textContent = characters[0].votes
-
-    //iterate through each character and add to the character bar
-    characters.forEach(character => addToCharacterBar(character))
-})
-//catch any errors and print in console
-.catch(err => console.log(err))
-
+     
+ /*
+  pseudocode 
+  grab the form using id
 const form = document.querySelector('#votes-form')
-
-form.addEventListener('submit', (e) => {
+ form.addEventListener('submit', function(e))
+ {
     e.preventDefault()
-    //convert current votes to an integer and the inputed vote to an integer
+    convert current votes to an integer and the inputed vote to an integer
     let currentVotes = parseInt(characterVotes.textContent, 10)
     let addedVotes = parseInt(e.target.votes.value, 10)
-    //add the inputed vote to the current votes and input that number into the textContent
-    characterVotes.textContent = (currentVotes += addedVotes)
+    add the inputed vote to the current votes and input that number into the textContent
+     characterVotes.textContent = (currentVotes += addedVotes)
     form.reset()
-
-    //Use PATCH to reset character votes in json file to adjusted votes^^
-    fetch(url)
-    .then(res => res.json())
-    .then(characters => {
-        const charName = document.querySelector('#name')
-        const charID = characters.find(character => character.name === charName.textContent)
+ 
+     const charID = characters.find(character => character.name === charName.textContent)
+         console.log(charID)
         fetch(`${url}/${charID.id}`, {
             method: 'PATCH',
             headers: {
@@ -60,86 +36,31 @@ form.addEventListener('submit', (e) => {
             },
             body: JSON.stringify({
                 votes : characterVotes.textContent
-            })
-        })
-        .then(res => res.json())
-        .then(data => {
-            characterVotes.textContent = data.votes
-            console.log(data.votes)
+           })
        })
-    })
-})
-
-
-//create function addToCharacterBar that takes each character as a parameter 
-const addToCharacterBar = (character) => {
-    //create a span element for each character and input their name into it, add that span to the character bar
-    const span = document.createElement('span')
-    span.textContent = character.name
-    characterBar.append(span)
-
-    //make an event listener that fires every time you click on a span element
-    span.addEventListener('click', (e) => {
-        
-        const name = characterInfo.querySelector('#name')
-        const image = characterInfo.querySelector('#image')
-
-        //when you click, it changes the character info to the character info of the span clicked
-        name.textContent = character.name
-        image.src = character.image
-    })     
-
-}
-
-
-
-const resetButton = document.querySelector('#reset-btn')
-
-resetButton.addEventListener('click', (e) => {
-
-    fetch(url)
-    .then(res => res.json())
-    .then(characters => {
-        const charName = document.querySelector('#name')
-        const charID = characters.find(character => character.name === charName.textContent)
-        fetch(`${url}/${charID.id}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify({
-                votes : '0'
-            })
-        })
-        .then(res => res.json())
+       .then(res => res.json())
         .then(data => characterVotes.textContent = data.votes)
     })
+})*/
 
-})
+/*explanation
+We need to create a backend Json server for the test
+The test requires an application that can accept an input which is a number to one of the animals.
+When you input a number in the input field in counts as a vote to the speciefied animal as the most 
+beautiful animal.
+The test also requires a reset button to clear all the votes to default*/
+const url = 'http://localhost:3000/characters'
+const characterBar = document.querySelector('#character-bar')
+const characterInfo = document.querySelector('#detailed-info')
+let characterVotes = document.querySelector('#vote-count')
 
-
-//Add new character using POST method
-const addNewForm = document.querySelector("#character-form")
-
-addNewForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const newName = e.target.name.value
-    const newImage = e.target['image-url'].value
-
-    fetch(url, {
-        method: "POST",
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify({
-            name : newName,
-            image : newImage,
-            votes : '0',
-        })
-    })
-    .then(res => res.json())
-    .then(data => addToCharacterBar(data))
-    .catch(err => console.log(err))
-
-    addNewForm.reset()
+fetch(url)
+.then(res => res.json())
+.then(characters => {
+ const name = characterInfo.querySelector('#name')
+    const image = characterInfo.querySelector('#image')
+    name.textContent = characters[0].name
+    image.src = characters[0].image
+    characterVotes.textContent = characters[0].votes
+ characters.forEach(character => addToCharacterBar(character))
 })
